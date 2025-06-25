af9744971e274219027a7cd9f1c5118fda027e64
Aric Williams <aricwilliamst@gmail.com>
2025-06-25 09:15:52 -0400

    fix

--- public/index.php
+++ public/index.php
@@ -5,16 +5,19 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+// Path to the Laravel core
+$laravelPath = __DIR__ . '/../laravel-api';
+
+// Maintenance mode check
+if (file_exists($laravelPath . '/storage/framework/maintenance.php')) {
+    require $laravelPath . '/storage/framework/maintenance.php';
 }
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+// Autoloader
+require $laravelPath . '/vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
+// Bootstrap the application
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once $laravelPath . '/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
